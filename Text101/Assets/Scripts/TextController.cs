07249996f1de1b0c4dfb4294268fe1e1d2b4ba89
Jordan Ashton <jordie922@gmail.com>
2016-11-09 17:49:57 +0000

    fixed error, added more story

--- Text101/Assets/Scripts/TextController.cs
+++ Text101/Assets/Scripts/TextController.cs
@@ -17,9 +17,13 @@ public class TextController : MonoBehaviour {
 	void Update () {
 		print (myState);
 		if(myState == States.cell) {
-			States.cell();
+			state_cell();
 		}else if (myState == States.sheets_0) {
-			States.sheets_0();
+			state_sheets_0();
+		}else if (myState == States.mirror) {
+			state_mirror();
+		}else if (myState == States.lock_0) {
+			state_lock_0();
 		}
 	}
 	
@@ -30,6 +34,10 @@ public class TextController : MonoBehaviour {
 					"Press S to view sheets. Pres M to view mirror and L to view lock";
 		if(Input.GetKeyDown(KeyCode.S)) {
 			myState = States.sheets_0;
+		} else if(Input.GetKeyDown(KeyCode.L)) {
+			myState = States.lock_0;
+		} else if(Input.GetKeyDown(KeyCode.M)) {
+			myState = States.mirror;
 		}
 	}
 	
@@ -41,5 +49,28 @@ public class TextController : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.R)) {
 			myState = States.cell;
 		}
+	}
+	
+	void state_mirror() {
+		text.text = "You have a look at the mirror. Damn im fine you think to yourself! "+
+					"The mirror is just hanging off the wall. Maybe you could take it? \n\n" + 
+					"Press R to return to the cell or T to take the mirror";
+		if(Input.GetKeyDown(KeyCode.R)) {
+			myState = States.cell;
+		} else if(Input.GetKeyDown(KeyCode.T)) {
+		/*
+			UNCOMMENT CODE WHEN "States.cell_mirror" HAS BEEN CREATED.		
+			myState = States.cell_mirror();
+		*/
+		}
+	}
+	
+	void state_lock_0() {
+		text.text = "Theres a lock on the outside. It's obviously the way into your cell. " + 
+					"If only there was a way to see the keypad on it.\n\n" +
+					"Press R to return to cell";
+					if(Input.GetKeyDown(KeyCode.R)) {
+						myState = States.cell;
+					}
 	}
 }
